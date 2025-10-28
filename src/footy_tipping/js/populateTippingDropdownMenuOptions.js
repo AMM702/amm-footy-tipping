019156025d4165bd8cc51681955556d117d0b96c
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-10-28 16:41:54 +1000

    Feature/populate tipping drop down menu options (#9)
    - Options are automatically populated into menu based on dynamic data
    - Existing games are cleared (including the submit button -> to be fixed in a future update)

---
+++ src/footy_tipping/js/populateTippingDropdownMenuOptions.js
@@ -0,0 +1,50 @@
+function populateTippingDropdownMenuOptions(gameData)
+{
+    const dropdownOptions = document.getElementById('tippingRoundListOptions');
+    const dropdownLabel = document.getElementById('tippingRoundDisplayText');
+    const dropdownMenu = document.getElementById('tippingRoundList');
+    const tippingForm = document.getElementById('tipping-form');
+
+    const slug = s => s.toLowerCase().trim().replace(/[\s\W]+/g, '-');
+
+    // Clear existing items
+    dropdownOptions.innerHTML = '';
+
+    // Add options to menu
+    for (const key in gameData)
+    {
+        if (gameData.hasOwnProperty(key)) 
+        {
+            const round = gameData[key];
+            
+            // Create drop down menu option
+            // Create drop down menu option
+            const a = document.createElement('a');
+            a.href = '#';
+            a.className = 'w-dropdown-link username_display';
+            a.textContent = round.name;
+            a.id = `${slug(round.name)}`;
+
+            // Add event listener
+            a.addEventListener('click', function (event)
+                {
+                    event.preventDefault();
+
+                    // Close the menu
+                    dropdownMenu.classList.remove('w--open');
+
+                    // Update the dropdown menu label
+                    dropdownLabel.textContent = round.name;
+
+                    // Create game elements
+                    tippingForm.innerHTML = ''; // Remove existing games
+                    round.matches.forEach(g => window.CustomLibrary.createGameFormElements(g));
+                }
+            );
+            dropdownOptions.appendChild(a);
+        }
+    };
+};
+
+window.CustomLibrary = window.CustomLibrary || {};
+window.CustomLibrary.populateTippingDropdownMenuOptions = populateTippingDropdownMenuOptions;
