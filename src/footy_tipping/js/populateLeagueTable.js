1dff8527894c57744c74c8bf2d4e2a218396bcb4
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-10-29 15:13:27 +1000

    Merge Feature/round league table into main (#12)
    Created the round league table

---
+++ src/footy_tipping/js/populateLeagueTable.js
@@ -0,0 +1,46 @@
+function populateLeagueTable(roundData)
+{
+    // Define element ID constants
+    
+    const roundResultsDiv = document.getElementById('round-results-div');
+
+    // Create table
+    const table = document.createElement('table');
+    table.classList.add('custom-table');
+
+    // Create header row 
+    const thead = table.createTHead();
+    const headerRow = thead.insertRow();
+
+    const headerUsername = document.createElement('th');
+    headerUsername.textContent = 'Username';
+    headerUsername.classList.add('custom-cell');
+    headerRow.appendChild(headerUsername);
+
+    const headerScore = document.createElement('th');
+    headerScore.textContent = 'Score';
+    headerScore.classList.add('custom-cell');
+    headerRow.appendChild(headerScore);
+
+    // Insert data
+    console.log(roundData);
+    roundData.forEach(item => {
+        console.log(item);
+        const row = table.insertRow();
+        row.classList.add('custom-row');
+
+        const usernameCell = row.insertCell();
+        usernameCell.textContent = item.username;
+        usernameCell.classList.add('custom-cell');
+
+        const scoreCell = row.insertCell();
+        scoreCell.textContent = item.score;
+        scoreCell.classList.add('custom-cell');
+    }
+    );
+
+    roundResultsDiv.appendChild(table);
+}
+
+window.CustomLibrary = window.CustomLibrary || {};
+window.CustomLibrary.populateLeagueTable = populateLeagueTable;
