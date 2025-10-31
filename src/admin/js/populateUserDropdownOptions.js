8b23e47cca283d527ed9ad6035d84dffb360efe8
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-10-31 07:05:57 +0000

    Populate user states dropdown menu with options

---
+++ src/admin/js/populateUserDropdownOptions.js
@@ -0,0 +1,38 @@
+function populateUserDropdownOptions() 
+{
+    // Select dropdown menu elements
+    const dropdownOptions = document.getElementById('users-dropdown-options');
+    const dropdownLabel = document.getElementById('users-dropdown-text');
+
+    // Define array of states
+    const states = ['QLD','NSW','VIC','SA'];
+
+    // Clear existing elements
+    dropdownOptions.innerHTML = '';
+    
+    // Add options to the menu
+    states.forEach(state => {
+        // Create option
+        const a = document.createElement('a');
+        a.href = '#';
+        a.className = 'w-dropdown-link username_display';
+        a.textContent = state;
+        a.id = state;
+
+        // Add event listener
+        a.addEventListener('click', function (event)
+            {
+                event.preventDefault();
+
+                // Change menu label
+                dropdownLabel.textContent = 'state';
+
+                // Change displayed table
+                // Add logic here
+            });
+        dropdownOptions.appendChild(a);
+    });
+};
+
+window.CustomLibrary = window.CustomLibrary || {};
+window.CustomLibrary.populateUserDropdownOptions = populateUserDropdownOptions;
