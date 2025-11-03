f6aadf4759218ee43cf04f468bc48114aa4143f7
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-11-03 16:15:44 +1000

    Feature/add admin drop down menu options and display table (#19)
    * Populate drop down menu with state options
    * Display user data in a table based on which state is selected

---
+++ src/admin/js/populateUserDropdownOptions.js
@@ -0,0 +1,44 @@
+function populateUserDropdownOptions(userData) 
+{
+    // Select dropdown menu elements
+    const dropdownOptions = document.getElementById('users-dropdown-options');
+    const dropdownLabel = document.getElementById('users-dropdown-text');
+
+    // Define array of states
+    const states = ['QLD','NSW','VIC','WA'];
+
+    // Clear existing elements
+    dropdownOptions.innerHTML = '';
+    
+    // Add options to the menu
+    states.forEach(state => {
+        // Create option
+        const a = document.createElement('a');
+        a.href = '#';
+        a.className = 'w-dropdown-link username_display';
+        a.textContent = state;
+        a.id = state;
+
+        // Select data
+        const stateData = userData[state];
+
+        // Add event listener
+        a.addEventListener('click', function (event)
+            {
+                event.preventDefault();
+
+                // Change menu label
+                dropdownLabel.textContent = state;
+
+                // Change displayed table
+                adminDisplayUserDetails(stateData);
+            });
+        dropdownOptions.appendChild(a);
+    });
+
+    // Default to QLD
+    adminDisplayUserDetails(userData['QLD']);
+};
+
+window.CustomLibrary = window.CustomLibrary || {};
+window.CustomLibrary.populateUserDropdownOptions = populateUserDropdownOptions;
