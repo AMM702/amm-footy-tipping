9dc2964c209fe147e54206b6576d29ba336fb0bb
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-11-06 06:11:51 +0000

    Display data in a table

--- src/admin/js/populateScoreDropdownMenu.js
+++ src/admin/js/populateScoreDropdownMenu.js
@@ -20,8 +20,11 @@ function populateScoreDropdownMenu(gameData, scoreForm, scoreLabel, scoreOptions
                 // Change menu label
                 roundLabel.textContent = `Round ${round.round}`;
 
+                // Clear existing form elements
+                formElement.innerHTML = '';
+
                 // Display round data
-                displayScoreData(round);
+                formElement.appendChild(displayScoreData(round));
             });
         roundOptions.appendChild(a);
     });
@@ -29,5 +32,83 @@ function populateScoreDropdownMenu(gameData, scoreForm, scoreLabel, scoreOptions
 
 function displayScoreData(roundData)
 {
+    // Create table 
+    const table = document.createElement('table');
+    table.classList.add('custom-table');
 
+    // Create header row 
+    const tableHeaders = ['Date','Game','Result'];
+    const thead = table.createTHead();
+    const headerRow = thead.insertRow();
+
+    tableHeaders.forEach(header => {
+        const headerElement = document.createElement('th');
+        headerElement.textContent = header;
+        headerElement.classList.add('custom-cell');
+        headerRow.appendChild(headerElement);
+    });
+
+    // Display data
+    roundData.matches.forEach(match => {
+        const row = table.insertRow();
+
+        // Date cell
+        const dateCell = row.insertCell();
+        dateCell.textContent = match.date;
+        dateCell.classList.add('custom-cell');
+
+        // Game cell
+        const gameCell = row.insertCell();
+        gameCell.textContent = `${match.homeTeam} vs ${match.awayTeam}`;
+        gameCell.classList.add('custom-cell');
+
+        // Result cell
+        const gameResult = createSelectFormMenu(match);
+        const resultCell = row.insertCell();
+        resultCell.appendChild(gameResult);
+        resultCell.classList.add('custom-cell');
+    });
+
+    return table;
+};
+
+function createSelectFormMenu(matchData)
+{
+    const select = document.createElement('select');
+    select.name = `${matchData.gameID}`;
+    select.required = true;
+
+    // Create default option
+    const defaultOption = document.createElement('option');
+    defaultOption.value = 'null';
+    defaultOption.textContent = 'No result';
+    select.appendChild(defaultOption);
+
+    // Create home team winner options 
+    const homeOption = document.createElement('option');
+    homeOption.value = 1;
+    homeOption.textContent = matchData.homeTeam;
+    select.appendChild(homeOption);
+
+    // Create away team winner options
+    const awayOption = document.createElement('option');
+    awayOption.value = 2;
+    awayOption.textContent = matchData.awayTeam;
+    select.appendChild(awayOption);
+
+    // Auto select an option
+    if (matchData.result === 1) // Home team
+    {
+        homeOption.selected = true;
+    }
+    else if (matchData.result === 2) // Away team
+    {
+        awayOption.selected = true;
+    }
+    else // No result
+    {
+        defaultOption.selected = true;
+    };
+
+    return select;
 }
