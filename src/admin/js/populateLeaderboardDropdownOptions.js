e78738ab6f1f6c8747d301f87b78bcadd29175da
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-11-07 00:54:50 +0000

    Populate dropdown menu options

---
+++ src/admin/js/populateLeaderboardDropdownOptions.js
@@ -0,0 +1,41 @@
+function populateLeaderboardDropdownOptions(leaderboardData)
+{
+    // Select dropdown menu elements
+    const dropdownOptions = document.getElementById('leaderboard-dropdown-options');
+    const dropdownLabel = document.getElementById('leaderboard-dropdown-text');
+
+    // Define array of states
+    const states = ['QLD','NSW','VIC','WA'];
+
+    // Clear existing elements
+    dropdownOptions.innerHTML = '';
+    
+    // Add options to the menu
+    states.forEach(state => {
+        // Create option
+        const a = document.createElement('a');
+        a.href = '#';
+        a.className = 'w-dropdown-link username_display';
+        a.textContent = state;
+        a.id = state;
+
+        // Select data
+        const stateData = leaderboardData[state];
+
+        // Add event listener
+        a.addEventListener('click', function (event)
+            {
+                event.preventDefault();
+
+                // Change menu label
+                dropdownLabel.textContent = state;
+
+                // Change displayed table
+                // Call function here
+            });
+        dropdownOptions.appendChild(a);
+    });
+
+    // Default to QLD
+    // Call function here
+}
