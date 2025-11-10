e76b11c54d9b080f7ed32925e5b78fc783e8a171
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-11-10 12:36:27 +1000

    Add sendTippingEmail function (#30)

---
+++ src/admin/js/sendTippingEmail.js
@@ -0,0 +1,31 @@
+function sendTippingEmail()
+{
+    const form = document.getElementById('email-users-form-main')
+    
+    // Default to ALL recipients
+    const defaultRadioButton = document.querySelector('input[name="Recipient"][value="All"]');
+    if (defaultRadioButton) defaultRadioButton.checked = true;
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        // Get email list
+        const recipient = form.querySelector("input[name='Recipient']:checked")?.ariaValueMax;
+        if (!recipient)
+        {
+            alert("Select a recipient");
+            return
+        }
+
+        // Get data from backend
+        data = ['user1@email.com','user2@email.com','user3@email.com']; // Example data. Replace with call to backend API
+
+        bccList = data.join(",");
+
+        const mailto = `mailto:?bcc=${bccList}`;
+        window.open(mailto);
+    });
+}
+
+window.CustomLibrary = window.CustomLibrary || {};
+window.CustomLibrary.sendTippingEmail = sendTippingEmail;
