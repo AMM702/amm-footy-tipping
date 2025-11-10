60b762416021bcf8d8abe62db017a0ef3be7c9fc
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-11-10 06:36:13 +0000

    Navigate user to tipping page on successful login

--- src/login/js/sendLoginFormToBackend.js
+++ src/login/js/sendLoginFormToBackend.js
@@ -9,7 +9,16 @@ function sendLoginFormToBackend()
         const data = Object.fromEntries(formData.entries());
 
         // Send data to the backend
-        console.table(data) // Output form data to console
+        console.table(data) // DEBUG: Output form data to console
+        const statusCode = 200
+        const JWT = 'header.payload.signature'
+
+        // Log the user in if successful response returned
+        if (statusCode == 200)
+        {
+            sessionStorage.setItem("jwt", JWT);
+            window.location.href = '/tipping';
+        }
     })
 };
 
