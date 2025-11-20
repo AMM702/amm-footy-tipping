ce000f3df7581c17affa6d069e6abbecc39fb2aa
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-11-20 11:28:36 +1000

    Feature/login page form intercept (#32)
    * Add form listener to login form
    
    * Add function to custom library
    
    * Update form ID
    
    * Navigate user to tipping page on successful login
    
    * Add form listener to login form
    
    * Add function to custom library
    
    * Update form ID
    
    * Navigate user to tipping page on successful login
    
    * Bundle and minify login page files
    
    * Update dist bundles
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/login/js/sendLoginFormToBackend.js
@@ -0,0 +1,26 @@
+export function sendLoginFormToBackend()
+{
+    const form = document.getElementById('wf-form-Password');
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData.entries());
+
+        // Send data to the backend
+        console.table(data) // DEBUG: Output form data to console
+        const statusCode = 200
+        const JWT = 'header.payload.signature'
+
+        // Log the user in if successful response returned
+        if (statusCode == 200)
+        {
+            sessionStorage.setItem("jwt", JWT);
+            window.location.href = '/tipping';
+        }
+    })
+};
+
+window.CustomLibrary = window.CustomLibrary || {};
+window.CustomLibrary.sendLoginFormToBackend = sendLoginFormToBackend;
