84dadc956c4946b20433296337128a9f7d788899
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-11-28 04:45:31 +0000

    Track example data files

---
+++ example data/catchUpGameData example.js
@@ -0,0 +1,46 @@
+catchUpGameData = {
+    rounds: [
+        {
+            round: 1,
+            catchUpGameID: 1,
+            matches: [
+                {
+                    gameID: 1,
+                    homeTeam: "Broncos",
+                    awayTeam: "Cowboys"
+                },
+                {
+                    gameID: 2,
+                    homeTeam: "Storm",
+                    awayTeam: "Eels"
+                },
+                {
+                    gameID: 3,
+                    homeTeam: "Sharks",
+                    awayTeam: "Cowboys"
+                }
+            ]
+        },
+        {
+            round: 2,
+            catchUpGameID: null,
+            matches: [
+                {
+                    gameID: 4,
+                    homeTeam: "Broncos",
+                    awayTeam: "Cowboys"
+                },
+                {
+                    gameID: 5,
+                    homeTeam: "Storm",
+                    awayTeam: "Eels"
+                },
+                {
+                    gameID: 6,
+                    homeTeam: "Sharks",
+                    awayTeam: "Cowboys"
+                }
+            ]
+        }
+    ]
+};
