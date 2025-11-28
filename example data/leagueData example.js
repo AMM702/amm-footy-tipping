7670e0542a695a966f66177c514aa687902c9c86
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-11-28 14:54:48 +1000

    Feature/optimise json v2 (#34)
    * Use new gameData format
    
    * Update round selection logic
    
    * Replace push dist build with a button
    
    * Revert previous commit
    
    * Update dist bundles
    
    * Fix for loop conditions
    
    * Update dist bundles
    
    * Track example data files
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ example data/leagueData example.js
@@ -0,0 +1,16 @@
+const leagueData = {
+    leagueResults: [
+        {
+            username: "Wagyu",
+            score: 23
+        },
+        {
+            username: "Arch",
+            score: 10
+        },
+        {
+            username: "Beef",
+            score: 2
+        }
+    ]
+};
