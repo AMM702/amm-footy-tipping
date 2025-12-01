64d33975822bda38099acf667e846338a1fc1c54
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-12-01 05:20:58 +0000

    Intercept round form submissions

--- src/admin/js/form_intercepts/initForms.js
+++ src/admin/js/form_intercepts/initForms.js
@@ -1,8 +1,10 @@
 // Import form intercepts
 import { editPasswordFormIntercept } from "./editPasswordFormIntercept";
+import { gameRoundsFormIntercept } from "../gameRoundsFormIntercept";
 
 // Import and call this function in index.js
 export function initForms()
 {
     editPasswordFormIntercept();
+    gameRoundsFormIntercept();
 };
---
+++ src/admin/js/gameRoundsFormIntercept.js
@@ -0,0 +1,22 @@
+export function gameRoundsFormIntercept()
+{
+    // Select round forms
+    const nrlRoundForm = document.getElementById('nrl-catchup-form-div');
+    const aflRoundForm = document.getElementById('afl-catchup-form-div');
+
+    // Intercept form submissions
+    interceptRoundForm(nrlRoundForm);
+    interceptRoundForm(aflRoundForm);
+}
+
+function interceptRoundForm(formObj)
+{
+    formObj.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(formObj);
+        const data = Object.fromEntries(formData.entries());
+
+        console.log(data);
+    })
+};
