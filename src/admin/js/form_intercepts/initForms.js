a0fe19619761a799b968a50029c8961bbd3aaf9a
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-12-01 05:31:49 +0000

    Intercept score forms

--- src/admin/js/form_intercepts/initForms.js
+++ src/admin/js/form_intercepts/initForms.js
@@ -1,10 +1,12 @@
 // Import form intercepts
 import { editPasswordFormIntercept } from "./editPasswordFormIntercept";
 import { gameRoundsFormIntercept } from "../gameRoundsFormIntercept";
+import { interceptScoreForms } from "../interceptScoreForms";
 
 // Import and call this function in index.js
 export function initForms()
 {
     editPasswordFormIntercept();
     gameRoundsFormIntercept();
+    interceptScoreForms();
 };
