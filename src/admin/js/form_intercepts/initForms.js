595dd04be72d8bb9f306bed3ca42f66d510c2cc6
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-12-01 04:47:19 +0000

    Intercept edit password form

---
+++ src/admin/js/form_intercepts/initForms.js
@@ -0,0 +1,8 @@
+// Import form intercepts
+import { editPasswordFormIntercept } from "./editPasswordFormIntercept";
+
+// Import and call this function in index.js
+export function initForms()
+{
+    editPasswordFormIntercept();
+};
