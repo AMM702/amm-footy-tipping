2ddb905141a3dd33dabc9b83c7c4d0b98df9740e
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-12-01 14:30:44 +1000

    Feature/tipping intercept (#37)
    * Call interceptTippingForm in index.js
    
    * Update dist bundles
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/footy_tipping/js/index.js
+++ src/footy_tipping/js/index.js
@@ -2,3 +2,6 @@ import "./CreateGameFormElements.js";
 import "./populateLeagueTable.js";
 import "./populateTippingDropdownMenuOptions.js";
 import "./interceptTippingForm.js";
+
+// Call startup functions
+window.CustomLibrary.interceptTippingForm();
