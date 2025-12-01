e2a971a84fa92d1f8e11fa326a29d16417688093
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-12-01 16:22:12 +1000

    Feature/admin forms intercept (#38)
    * Intercept edit password form
    
    * Update dist bundles
    
    * Ignore dist/ files on merge
    
    * Intercept round form submissions
    
    * Update dist bundles
    
    * Remove turnstile form field
    
    * Update dist bundles
    
    * Intercept score forms
    
    * Update dist bundles
    
    * Update form reference
    
    * Update dist bundles
    
    * Update radio button value retrieval logic
    
    * Update dist bundles
    
    * Intercept data upload form
    
    * Update dist bundles
    
    * Fix typo
    
    * Update dist bundles
    
    * Remove turnstile from form data
    
    * Update dist bundles
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/admin/js/index.js
+++ src/admin/js/index.js
@@ -6,3 +6,7 @@ import "./populateLeaderboardDropdownOptions.js";
 import "./populateScoreDropdownMenu.js";
 import "./populateUserDropdownOptions.js";
 import "./sendTippingEmail.js";
+import { initForms } from "./form_intercepts/initForms.js";
+
+// Add form intercepts
+initForms();
