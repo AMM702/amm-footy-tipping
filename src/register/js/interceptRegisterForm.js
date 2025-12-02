3e61f6212f3b018c92cb0d30bec39d1f80444a90
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-12-02 11:59:10 +1000

    Feature/intercept register form (#39)
    * Intercept register form
    
    * Update dist bundles
    
    * Fix typo
    
    * Update dist bundles
    
    * Remove turnstile and throw error if no comp is selected
    
    * Update dist bundles
    
    * Navigate to login page on successful signin
    
    * Update dist bundles
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/register/js/interceptRegisterForm.js
@@ -0,0 +1,23 @@
+export function interceptRegisterForm()
+{
+    const form = document.getElementById('wf-form-register-user');
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+        formData.delete("cf-turnstile-response");
+        const data = Object.fromEntries(formData.entries());
+
+        if (data.aflComp == "None" && data.nrlComp == "None")
+        {
+            alert("You must select a competition.");
+            throw new Error("No competition was selected.");
+        }
+
+        console.log(data);
+
+        // Navigate to login page
+        window.location.href = '/';
+    });
+}
