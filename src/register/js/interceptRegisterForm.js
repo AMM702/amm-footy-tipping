f952fafaa4f50f584924740d4aedd152282cd13d
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-12-02 01:46:35 +0000

    Remove turnstile and throw error if no comp is selected

--- src/register/js/interceptRegisterForm.js
+++ src/register/js/interceptRegisterForm.js
@@ -6,7 +6,14 @@ export function interceptRegisterForm()
         e.preventDefault();
 
         const formData = new FormData(form);
+        formData.delete("cf-turnstile-response");
         const data = Object.fromEntries(formData.entries());
+
+        if (data.aflComp == "None" && data.nrlComp == "None")
+        {
+            alert("You must select a competition.");
+            throw new Error("No competition was selected.");
+        }
 
         console.log(data);
     });
