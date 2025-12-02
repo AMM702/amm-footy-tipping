f2b7c71efd5a728704bc5cb482a69f43f4d9cfd4
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-12-02 22:03:05 +0000

    Replace neither input with the ability to unselect the radio button

--- src/register/js/setDefaultStates.js
+++ src/register/js/setDefaultStates.js
@@ -1,9 +1,43 @@
 export function setDefaultStates()
 {
-    // Select radio buttons
-    const nrlButton = document.getElementById('none-nrl');
-    const aflButton = document.getElementById('none-afl');
+    // Select NRL Radio Buttons
+    const nrlButtons = [document.getElementById('QLD-register'), document.getElementById('NSW-register')];
+    const aflButtons = [document.getElementById('VIC-register'), document.getElementById('WA-register')];
+    
+    // Select event divs
+    nrlDiv = document.getElementById('nrl-comp-div');
+    aflDiv = document.getElementById('afl-comp-div');
+    
+    // Add hidden input
+    addRadioButtonListeners(nrlButtons, 'nrlComp', nrlDiv);
+    addRadioButtonListeners(aflButtons, 'aflComp', aflDiv);
+}
 
-    nrlButton.checked = true;
-    aflButton.checked = true;
+function addRadioButtonListeners(buttonsArray, groupName, div)
+{
+    // Create hidden input
+    const hiddenInput = document.createElement('input');
+    hiddenInput.type = 'hidden';
+    hiddenInput.name = groupName;
+    hiddenInput.value = 'none';
+    hiddenInput.id = `hidden_${groupName}`;
+    div.appendChild(hiddenInput);
+
+    // Add event listener
+    buttonsArray.forEach(radio => {
+        radio.addEventListener('click', () => {
+            if (radio.previousChecked)
+            {
+                radio.checked = false;
+                radio.previousChecked = false;
+                hiddenInput.value = 'none';
+            }
+            else 
+            {
+                document.querySelectorAll(`input[name="${groupName}"]`).forEach(r => r.previousChecked = false)
+                radio.previousChecked = true;
+                hiddenInput.value = radio.value;
+            }
+        });
+    });
 }
