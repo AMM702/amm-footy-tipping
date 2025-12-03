d9a08772a5beffc6e9356d750b5003dfdbf6a42c
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-12-03 11:58:44 +1000

    Feature/tipping comp buttons (#42)
    Create buttons to switch between different comps on the footy tipping page

--- example data/gameData example.js
+++ example data/gameData example.js
@@ -1,5 +1,5 @@
 const gameData = {
-    data: [
+    qld: [
         {
             round: 1,
             isRoundOn: false,
@@ -40,6 +40,152 @@ const gameData = {
                     userTip: 2,
                     isTippable: false
                 },
+                {
+                    gameID: 4657,
+                    gameNumber: 23,
+                    teams: { home: 'Bulldogs', away: 'Knights' },
+                    ground: 'McDonald Jones Stadium',
+                    time: '5:15PM',
+                    date: '29/09/2025',
+                    result: null,
+                    isCatchUp: false,
+                    userTip: null,
+                    isTippable: false
+                },
+                {
+                    gameID: 4321,
+                    gameNumber: 24,
+                    teams: { home: 'Storm', away: 'Sea Eagles' },
+                    ground: 'AAMI Park',
+                    time: '7:00PM',
+                    date: '30/09/2025',
+                    result: null,
+                    isCatchUp: false,
+                    userTip: null,
+                    isTippable: true
+                },
+                {
+                    gameID: 3241,
+                    gameNumber: 25,
+                    teams: { home: 'Raiders', away: 'Titans' },
+                    ground: 'GIO Stadium',
+                    time: '6:00PM',
+                    date: '01/10/2025',
+                    result: null,
+                    isCatchUp: false,
+                    userTip: 1,
+                    isTippable: true
+                },
+                {
+                    gameID: 8765,
+                    gameNumber: 26,
+                    teams: { home: 'Dragons', away: 'Warriors' },
+                    ground: 'Win Stadium',
+                    time: '3:30PM',
+                    date: '02/10/2025',
+                    result: null,
+                    isCatchUp: false,
+                    userTip: 2,
+                    isTippable: true
+                },
+                {
+                    gameID: 5678,
+                    gameNumber: 27,
+                    teams: { home: 'Sharks', away: 'Panthers' },
+                    ground: 'Shark Park',
+                    time: '8:00PM',
+                    date: '03/10/2025',
+                    result: null,
+                    isCatchUp: false,
+                    userTip: null,
+                    isTippable: true
+                },
+                {
+                    gameID: 1357,
+                    gameNumber: 28,
+                    teams: { home: 'Rabbitohs', away: 'Roosters' },
+                    ground: 'ANZ Stadium',
+                    time: '7:30PM',
+                    date: '04/10/2025',
+                    result: null,
+                    isCatchUp: false,
+                    userTip: 1,
+                    isTippable: true
+                }
+            ],
+            scores: [
+                {
+                    username: "Wagyu",
+                    score: 18
+                },
+                {
+                    username: "Beef",
+                    score: 2
+                },
+                {
+                    username: "Chicken",
+                    score: 0
+                }
+            ]
+        },
+        {
+            round: 2,
+            isRoundOn: true,
+            matches: [
+                {
+                    gameID: 4978,
+                    gameNumber: 29,
+                    teams: { home: 'Rabbitohs', away: 'Roosters' },
+                    ground: 'ANZ Stadium',
+                    time: '9:30PM',
+                    date: '10/10/2025',
+                    result: 2,
+                    isCatchUp: false,
+                    userTip: 2,
+                    isTippable: false
+                }
+            ],
+            scores: [
+                {
+                    username: "Wagyu",
+                    score: 2
+                }
+            ]
+        },
+        {
+            round: 3,
+            isRoundOn: true,
+            matches: [
+                {
+                    gameID: 1648,
+                    gameNumber: 30,
+                    teams: { home: 'Broncos', away: 'Rabbitohs' },
+                    ground: 'Picard Stadium',
+                    time: '10:30PM',
+                    date: '24/10/2025',
+                    result: null,
+                    isCatchUp: false,
+                    userTip: null,
+                    isTippable: true
+                }
+            ],
+            scores: [
+                {
+                    username: "Wagyu",
+                    score: 25
+                },
+                {
+                    username: "Beef",
+                    score: 5
+                }
+            ]
+        }
+    ],
+    nsw: [
+        {
+            round: 1,
+            isRoundOn: false,
+            matches: [
                 {
                     gameID: 4657,
                     gameNumber: 23,
--- src/footy_tipping/js/index.js
+++ src/footy_tipping/js/index.js
@@ -2,6 +2,7 @@ import "./CreateGameFormElements.js";
 import "./populateLeagueTable.js";
 import "./populateTippingDropdownMenuOptions.js";
 import "./interceptTippingForm.js";
+import "./createGameTypeButtons.js";
 
 // Call startup functions
 window.CustomLibrary.interceptTippingForm();
