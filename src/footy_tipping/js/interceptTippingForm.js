ae2048fcdb37f081bd8dac1d155fa7274abe32d0
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-12-04 11:46:32 +1000

    Feature/identify comp (#45)
    Identify the comp that the user is submitting tips for. Modify JSON form structure to easily identify the comp and the user's tips.

--- src/footy_tipping/js/interceptTippingForm.js
+++ src/footy_tipping/js/interceptTippingForm.js
@@ -9,7 +9,29 @@ export function interceptTippingForm()
         formData.delete("cf-turnstile-response");
         const data = Object.fromEntries(formData.entries());
 
-        console.table(data);
+        let jsonData = {
+            comp: data.comp,
+            games: {}
+        }
+
+        for (let key in data)
+        {
+            if (!data.hasOwnProperty(key)) continue;
+
+            let value = data[key];
+
+            if (key != 'comp')
+            {
+                jsonData.games[key] = value;
+            }
+        }
+
+        // Convert jsonData into a string that can be sent to the backend
+        const jsonString = JSON.stringify(jsonData);
+
+        console.log(jsonData);
+        console.log(jsonString);
+        
     });
 };
 
