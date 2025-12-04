53385533f1fa70d7dc556c00185d0094adeda853
AMM702 <240384042+AMM702@users.noreply.github.com>
2025-12-04 01:29:35 +0000

    Intercept form now converts the data into a unique json structure

--- src/footy_tipping/js/interceptTippingForm.js
+++ src/footy_tipping/js/interceptTippingForm.js
@@ -9,8 +9,23 @@ export function interceptTippingForm()
         formData.delete("cf-turnstile-response");
         const data = Object.fromEntries(formData.entries());
 
+        let jsonData = {
+            comp: data.comp,
+            games: {}
+        }
+
+        for (let key in data)
+        {
+            let value = data[key];
+            if (key != 'comp')
+            {
+                jsonData.games[key] = value;
+            }
+        }
+
         console.table(data);
         console.log(data);
+        console.log(jsonData)
     });
 };
 
